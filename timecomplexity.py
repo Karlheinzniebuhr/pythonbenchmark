2cd50ddda6d8eb5530d408eab311626852651b00
Karlheinz Niebuhr <karlheinz.niebuhr@gmx.com>
2015-04-25 22:50:29 -0400

    adapted module for imports

--- timecomplexity.py
+++ timecomplexity.py
@@ -1,47 +1,35 @@
-# copyright Karlheinz Niebuhr
+# Copyright Karlheinz Niebuhr
 import time                         #  This part of code is to calculate how long a 
-def time_execution(code):           #  code will take to run (learned in class). 
+def time_execution(function, arg):           #  code will take to run (learned in class). 
     start = time.clock()
-    result = eval(code)
+    function( arg )
     run_time = time.clock() - start
     return run_time
-
-def sillycaseCaffeine(string):
-    return string[:int(round(len(string) / 2))].lower() + string[int(round(len(string) / 2)):].upper()
-
-def sillycaseKarl(string):
-    half = int(round(len(string) / 2))
-    return string[:half].lower() + string[half:].upper()
 
 
 def avgof(times,function, parameter):
     count = 0
-    track = 0 
-    track1 = 0
-    track2 = 0
-    track3 = 0
-    to_execute1 = function + "('abcdefgh')"
-    to_execute2 = function + "('abcdefghi')"
-    to_execute3 = function + "('ghi')"
-    while count < times:
-        track1 += time_execution(to_execute1)
-        track2 += time_execution(to_execute2)
-        track3 += time_execution(to_execute3)
-        count += 1
-    return (track1+track2+track3)/times
+    track = 0
 
-# Finally we check which one is faster and print!
-i = 0      
-a = 0
-b = 0                      
-while i < 10:
-    first = avgof(1000, "sillycaseKarl")
-    second = avgof(1000, "sillycaseCaffeine")
-    if first < second:
-        print 'sillycaseKarl is ' + str(second/first) + ' times faster'
-        a+=1
-    else:
-        print 'sillycaseCaffeine is ' + str(first/second) + ' times faster'
-        b+=1
-    i += 1
-print "Average is: a="+str(a)+' b='+str(b) 
+    while count < times:
+        track += time_execution(function, parameter)
+        count += 1
+    return track/times
+
+def compare(functionA, functionB, parameter, times, loops=10,):
+    # loop n times
+    i = 0
+    a = 0
+    b = 0                      
+    while i < loops:
+        first = avgof(times, functionA, parameter)
+        second = avgof(times, functionB, parameter)
+        if first < second:
+            print 'functionA is ' + str(second/first) + ' times faster'
+            a+=1
+        else:
+            print 'functionB is ' + str(first/second) + ' times faster'
+            b+=1
+        i += 1
+    print "Average is: a="+str(a)+' b='+str(b)
+
