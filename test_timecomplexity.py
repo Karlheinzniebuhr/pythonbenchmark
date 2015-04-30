a281f582f9842b9d2a6d625d8c3db82852b3ae79
Karlheinz Niebuhr <karlheinz.niebuhr@gmx.com>
2015-04-30 12:13:47 -0400

    added test function

---
+++ test_timecomplexity.py
@@ -0,0 +1,21 @@
+from timecomplexity import compare, measure
+import time
+
+
+
+a,b,c,d,e = 10,10,10,10,10
+
+# @measure
+def myFunction(a,b,c,d,e):
+	time.sleep(0.4)
+
+# @measure
+def myOptimizedFunction(a,b,c,d,e):
+	time.sleep(0.2)
+
+# decorator test
+#loop(input)
+#square2(input)
+
+# comparing test
+compare(myFunction, myOptimizedFunction, 2,a,b,c,d,e)
