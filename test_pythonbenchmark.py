ace65619b7982b948f9cbd52f31c97be4c937a41
Karlheinz Niebuhr <karlheinz.niebuhr@gmx.com>
2015-05-02 13:39:21 -0400

    clarified test

--- test_pythonbenchmark.py
+++ test_pythonbenchmark.py
@@ -1,21 +1,20 @@
 from pythonbenchmark import compare, measure
 import time
 
-
-
 a,b,c,d,e = 10,10,10,10,10
+something = [a,b,c,d,e]
 
-# @measure
-def myFunction(a,b,c,d,e):
+@measure
+def myFunction(something):
 	time.sleep(0.4)
 
-# @measure
-def myOptimizedFunction(a,b,c,d,e):
+@measure
+def myOptimizedFunction(something):
 	time.sleep(0.2)
 
 # decorator test
-#loop(input)
-#square2(input)
+myFunction(input)
+myOptimizedFunction(input)
 
 # comparing test
-compare(myFunction, myOptimizedFunction, 2,a,b,c,d,e)
+# compare(myFunction, myOptimizedFunction, 2,a,b,c,d,e)
