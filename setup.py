f5c06abbc98200b3607d2c31bebe9b61a113ff10
Karlheinz Niebuhr <karlheinz.niebuhr@gmx.com>
2015-08-25 18:31:39 -0400

    uploaded to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pythonbenchmark',
+  packages = ['pythonbenchmark'], # this must be the same as the name above
+  version = '1.0',
+  description = 'A Python library that makes benchmarking easy and fun.',
+  author = 'Karlheinz Niebuhr',
+  author_email = 'karlheinzniebuhr89@gmail.com',
+  url = 'https://github.com/Karlheinzniebuhr/pythonbenchmark', # use the URL to the github repo
+  download_url = 'https://github.com/Karlheinzniebuhr/pythonbenchmark.git', # I'll explain this in a second
+  keywords = ['benchmark', 'python', 'optimization'], # arbitrary keywords
+  classifiers = [],
+)
